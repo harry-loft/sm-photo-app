98c9d01979e43f767fef1034976cb62b617744de
Harry Loft <harry.loft@me.com>
2020-04-09 20:49:05 +0100

    Add Sendgrid and user confirmation email.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => Rails.application.credentials.sendgrid[:SENDGRID_USERNAME],
+  :password => Rails.application.credentials.sendgrid[:SENDGRID_PASSWORD],
+  :domain => 'yourdomain.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 465,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
